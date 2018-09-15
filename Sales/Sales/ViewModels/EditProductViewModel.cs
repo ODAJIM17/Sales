e4fde2d4f66a8388d8a36a4244f31fb195f7bdf9
ODAJIM17 <davila56jd@gmail.com>
2018-09-15 08:34:41 -0400

    api edit products

--- Sales/Sales/ViewModels/EditProductViewModel.cs
+++ Sales/Sales/ViewModels/EditProductViewModel.cs
@@ -2,9 +2,14 @@
 
 namespace Sales.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Plugin.Media;
     using Plugin.Media.Abstractions;
     using Sales.Common.Models;
+    using Sales.Helpers;
     using Sales.Services;
+    using System.Linq;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
     public class EditProductViewModel : BaseViewModel
@@ -56,6 +61,150 @@ namespace Sales.ViewModels
         }
         #endregion
 
-       
+
+        #region Commands
+
+        public ICommand ChangeImageCommand
+        {
+            get
+            {
+                return new RelayCommand(ChangeImage);
+            }
+        }
+
+        private async void ChangeImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            var source = await Application.Current.MainPage.DisplayActionSheet(
+                Languages.ImageSource,
+                Languages.Cancel,
+                null,
+                Languages.FromGallery,
+                Languages.NewPicture);
+
+            if (source == Languages.Cancel)
+            {
+                this.file = null;
+                return;
+            }
+
+            if (source == Languages.NewPicture)
+            {
+                this.file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        Directory = "Sample",
+                        Name = "test.jpg",
+                        PhotoSize = PhotoSize.Small,
+                    }
+                );
+            }
+            else
+            {
+                this.file = await CrossMedia.Current.PickPhotoAsync();
+            }
+
+            if (this.file != null)
+            {
+                this.ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = this.file.GetStream();
+                    return stream;
+                });
+            }
+        }
+
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new RelayCommand(Save);
+            }
+        }
+
+        private async void Save()
+        {
+            if (string.IsNullOrEmpty(this.Product.Description))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.DescriptionError,
+                    Languages.Accept);
+                return;
+            }
+
+           
+
+          
+            if (this.Product.Price < 0)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                   Languages.Error,
+                   Languages.PriceError,
+                   Languages.Accept);
+                return;
+            }
+
+            this.IsEnabled = false;
+            this.IsRunning = true;
+
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsEnabled = true;
+                this.IsRunning = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    connection.Message,
+                    Languages.Accept);
+                return;
+            }
+
+            byte[] imageArray = null;
+            if (this.file != null)
+            {
+                imageArray = FilesHelper.ReadFully(this.file.GetStream());
+                this.product.ImageArray = imageArray;
+            }
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlProductsController"].ToString();
+            var response = await this.apiService.Put(url, prefix, controller, this.product, this.product.ProductId);
+
+            // the code below also works when adding a new product. just uncomment the url  and the response vars.
+            // var url = Application.Current.Resources["UrlAPI"].ToString();
+            // var response = await this.apiService.Post(url, "/api", "/Products", product);
+
+            if (!response.IsSuccess)
+            {
+                this.IsEnabled = true;
+                this.IsRunning = false;
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
+                return;
+            }
+
+            var newProduct = (Product)response.Result;
+            var productsViewModel = ProductsViewModel.GetInstance();
+
+            
+            var oldProduct = productsViewModel.MyProducts.Where(p => p.ProductId == this.Product.ProductId).FirstOrDefault();
+            if (oldProduct != null)
+            {
+                productsViewModel.MyProducts.Remove(oldProduct);
+            }
+
+            productsViewModel.MyProducts.Add(newProduct);
+            productsViewModel.RefreshList();
+
+            this.IsEnabled = true;
+            this.IsRunning = false;
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
     }
+
+    #endregion
+
 }
+
