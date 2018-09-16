5ac8c7340c8b3926fbb307d5a271b7f896a2ba92
ODAJIM17 <davila56jd@gmail.com>
2018-09-16 14:02:23 -0400

    SQLite
    Added the sqlite database

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -9,6 +9,7 @@ namespace Sales.ViewModels
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
+    using System.Threading.Tasks;
     using System.Windows.Input;
     using Xamarin.Forms;
 
@@ -17,8 +18,13 @@ namespace Sales.ViewModels
         #region Attributes
 
         private ApiService apiService;
+
+        private DataService dataService;
+
         private ObservableCollection<ProductItemViewModel> products;
+
         private bool isRefreshing;
+
         private string filter;
 
         #endregion
@@ -55,6 +61,7 @@ namespace Sales.ViewModels
         public ProductsViewModel()
         {
             instance = this;
+            this.dataService = new DataService();
             this.apiService = new ApiService();
             this.LoadProducts();
         }
@@ -85,35 +92,54 @@ namespace Sales.ViewModels
             this.IsRefreshing = true;
 
             var connection = await this.apiService.CheckConnection();
-            if (!connection.IsSuccess)
+            if (connection.IsSuccess)
+            {
+                var answer = await this.LoadProductsFromAPI();
+                if (answer)
+                {
+                    this.SaveProductsToDB();
+                }
+            }
+            else
+            {
+                await this.LoadProductsFromDB();
+            }
+
+            if (this.MyProducts == null || this.MyProducts.Count == 0)
             {
                 this.IsRefreshing = false;
-                await Application.Current.MainPage.DisplayAlert(Languages.Error, connection.Message, Languages.Accept);
-                // await Application.Current.MainPage.DisplayAlert("Error!", "Please Check your internet settings", "Ok");
-
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, Languages.NoProductsMessage, Languages.Accept);
                 return;
             }
 
-            var url = Application.Current.Resources["UrlAPI"].ToString();
-            var response = await this.apiService.GetList<Product>(
-               url,
-                "/api",
-                "/Products",
-                Settings.TokenType,
-                Settings.AccessToken);
+            this.RefreshList();
+            this.IsRefreshing = false;
+        }
 
+        private async Task LoadProductsFromDB()
+        {
+            this.MyProducts = await this.dataService.GetAllProducts();
+        }
+
+        private async Task SaveProductsToDB()
+        {
+            await this.dataService.DeleteAllProducts();
+            this.dataService.Insert(this.MyProducts);
+        }
+
+        private async Task<bool> LoadProductsFromAPI()
+        {
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlProductsController"].ToString();
+            var response = await this.apiService.GetList<Product>(url, prefix, controller, Settings.TokenType, Settings.AccessToken);
             if (!response.IsSuccess)
             {
-                this.IsRefreshing = false;
-                await Application.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
-                return;
+                return false;
             }
 
             this.MyProducts = (List<Product>)response.Result;
-            this.RefreshList();
-            this.IsRefreshing = false;
-
-
+            return true;
         }
 
         public void RefreshList()
