6b2ee5391854806f102824a73493260170b31f36
ODAJIM17 <davila56jd@gmail.com>
2018-09-23 13:06:46 -0400

    Splash screen
    Created the splash screen and icons for IOS and Android

--- Sales/Sales.Android/MainActivity.cs
+++ Sales/Sales.Android/MainActivity.cs
@@ -3,13 +3,14 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using ImageCircle.Forms.Plugin.Droid;
 using Plugin.CurrentActivity;
 using Plugin.Permissions;
 
 namespace Sales.Droid
 {
     [Activity(Label = "Sales", 
-        Icon = "@mipmap/icon", 
+        Icon = "@drawable/ic_launcher", 
         Theme = "@style/MainTheme",
         MainLauncher = true, 
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -22,6 +23,7 @@ namespace Sales.Droid
 
             base.OnCreate(savedInstanceState);
             CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            ImageCircleRenderer.Init();
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
